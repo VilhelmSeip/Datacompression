4423db501f3849e970d45e9c9fa5cf4135b25af7
Vilhelm Seip <georgvs@stud.ntnu.no>
2020-10-19 15:48:32 +0200

    Everything is fucked, started over

--- Lempel-Ziv Compression/src/LempelZivCompression.java
+++ Lempel-Ziv Compression/src/LempelZivCompression.java
@@ -8,173 +8,77 @@ import java.util.Arrays;
 public class LempelZivCompression {
 
     private byte[] bytesInFile;
-    private byte[] compressed_bytes;
-    private byte[] tempBlock;
+    private byte[] dictionary;
+    private ArrayList<String> temp = new ArrayList<>();
+    private String fileIn;
     private String fileOut;
-    private final int distance_back;
-    private final int wordMinSize;
-    private int bytesLeft;
 
 
-    public LempelZivCompression(){
-        this.bytesInFile = new byte[0];
-        this.distance_back = 127;
-        this.wordMinSize = 4;
-    }
 
-    public LempelZivCompression(String inputFile, String fileOut, int distance_back, int wordMinSize) throws IOException {
+    public LempelZivCompression(String fileIn, String fileOut) throws IOException {
         this.bytesInFile = new byte[0];
         this.fileOut = fileOut;
-        this.distance_back = distance_back;
-        this.wordMinSize = wordMinSize;
-        readFile(inputFile);
+        readFile(fileIn);
     }
+
+
 
     private void readFile(String file) throws IOException {
         try {
             bytesInFile = Files.readAllBytes(Paths.get(file));
-            compressed_bytes = new byte[bytesInFile.length];
+            dictionary = new byte[bytesInFile.length];
         }catch (IOException e){
             e.printStackTrace();
         }
     }
 
-    private int locateSimilarByte(ArrayList<Byte> bytes, int startIndex){
-
-        int whereToStart = startIndex - distance_back;
-        int howFar = distance_back;
-        if (whereToStart < 0){
-            whereToStart = 0;
-            howFar = startIndex;
-        }
-
-        for (int i = whereToStart; i <= (howFar - wordMinSize); i++){
-            boolean located = true;
-            for (int j = i, k = 0; k < distance_back; j++, k++) {
-                try {
-                    if (tempBlock[j] != bytes.get(k)) {
-                        located = false;
-                        break;
-                    }
-                }catch (Exception e){ }
-            }
-
-            if (located){
-                return i;
+    private ArrayList<Byte> getBlock(int blockSize){
+        ArrayList<Byte> block = new ArrayList<>();
+        if (blockSize <= bytesInFile.length) {
+            for (int i = 0; i < blockSize; i++) {
+                block.add(bytesInFile[i]);
+                bytesInFile = removeArrayElement(bytesInFile, i);
             }
         }
-        return -2;
+        return block;
     }
 
-    private void getNewBlock(int bytesLeft){
-        int get = 0;
-        if (bytesLeft < distance_back){
-            tempBlock = new byte[bytesLeft];
-            for (int i = 0; i < bytesLeft; i++, get++){
-                tempBlock[i] = bytesInFile[get];
-            }
-            this.bytesLeft = 0;
-        }else {
-            tempBlock = new byte[distance_back];
-            for (int i = 0; i < distance_back; i++, get++){
-                tempBlock[i] = bytesInFile[get];
-            }
-            this.bytesLeft -= distance_back;
+    private  byte[] removeArrayElement(byte[] array, int index){
+        byte[] newArray = new byte[array.length - 1];
+        for (int i = 0, k = 0; i < array.length; i++) {
+            if (i == index) continue;
+
+            newArray[k++] = array[i];
         }
+
+        return newArray;
     }
 
-    public void compress(){
-
-        bytesLeft = bytesInFile.length;
-        int bufferindex = 0;
-
-        while (bytesLeft != 0){
-
-            getNewBlock(bytesLeft);
-            int byteIndex = 0;
-            boolean compressionFound = false;
-            int compressionIndex = -2;
-            int bytesFinished = 0;
-
-            for (int i = 0; i < bytesInFile.length; i++) {
-
-                ArrayList<Byte> presentBytes = new ArrayList<>();
-
-                compressionFound = false;
-                int compressionLength = -2;
-                int uncompressedBytes = -2;
-                int compressionStartIndex = 0;
-
-                for (int j = 0; j < bytesInFile.length; j++) {
-                    presentBytes.add(bytesInFile[j]);
-
-                    if (presentBytes.size() >= wordMinSize && i >= wordMinSize && bytesInFile.length - i >= wordMinSize) {
-
-                        int compressionPlace = locateSimilarByte(presentBytes, i);
-                        if (compressionPlace >= 0) {
-                            compressionFound = true;
-                            compressionIndex = i;
-                            compressionStartIndex = compressionPlace;
-                            compressionLength = presentBytes.size();
-                        } else break;
-                    }
-                }
-
-
-                if (compressionFound) {
-                    int uncompressed = compressionIndex - bytesFinished;
-                    compressed_bytes[bufferindex] = (byte)uncompressed;
-
-                    bufferindex++;
-
-                    for (int h = bytesFinished; h < compressionIndex; h++, bufferindex++) {
-                        compressed_bytes[bufferindex] = bytesInFile[h];
-                    }
-                    int back = compressionIndex -compressionStartIndex;
-
-                    compressed_bytes[bufferindex] = (byte) back;
-                    bufferindex++;
-                    compressed_bytes[bufferindex] = (byte) compressionLength;
-                    i += compressionLength;
+    public void findSimilarBytes(int howFar){
+        ArrayList<Byte> bytesToCheck = getBlock(howFar);
+        String check = bytesToCheck.get(0).toString();
+        for (int i = 0; i < bytesToCheck.size(); i++) {
+            boolean found = false;
+            for (int j = 0; j < temp.size(); j++) {
+                if (check != temp.get(j)){
+                    temp.add(check);
+                    check = bytesToCheck.get(i).toString();
+                    found = true;
+                    break;
                 }
             }
-
-            int uncompressed = bytesInFile.length - bytesFinished;
-
-            compressed_bytes[bufferindex] = (byte) - uncompressed;
-            bufferindex++;
-
-            for (int y = bytesFinished; y < bytesInFile.length; y ++){
-                compressed_bytes[bufferindex] = bytesInFile[y];
+            if (!found){
+                check = check + bytesToCheck.get(i + 1).toString();
             }
         }
-
-        byte[] buffer = compressed_bytes;
-        emptyBufferBytesFix(buffer, bufferindex);
-        writeToFile();
     }
 
-    private void writeToFile() {
-        try{
-            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(fileOut));
-            dataOutputStream.write(compressed_bytes);
-            dataOutputStream.close();
-        } catch (IOException e){
-            e.printStackTrace();
+    public void print(){
+        for (int i = 0; i < bytesInFile.length; i++) {
+            System.out.println("Byte: " + bytesInFile[i] + "\n");
+            char ch = (char)bytesInFile[i];
+            System.out.println("char: " + ch + "\n");
         }
-    }
-
-    private void emptyBufferBytesFix(byte[] buffer, int bufferLength) {
-        compressed_bytes = new byte[bufferLength];
-        for (int i = 0; i < bufferLength; i++) {
-            compressed_bytes[i] = buffer[i];
-        }
-    }
-
-    public void compress(String inputFile, String fileOut) throws IOException {
-        this.fileOut = fileOut;
-        readFile(inputFile);
-        compress();
     }
 
 
--- Lempel-Ziv Compression/src/Main.java
+++ Lempel-Ziv Compression/src/Main.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        LempelZivCompression lempelZivCompression = new LempelZivCompression("C:\\Users\\Vilhe\\IdeaProjects\\Oblig7\\Lempel-Ziv Compression\\lib\\test2.pdf",
-                "C:\\Users\\Vilhe\\IdeaProjects\\Oblig7\\Lempel-Ziv Compression\\lib\\FileOutTest.txt", 127, 4);
+        LempelZivCompression lempelZivCompression = new LempelZivCompression("C:\\Users\\Vilhe\\IdeaProjects\\Oblig7\\Lempel-Ziv Compression\\lib\\Test.txt",
+                "C:\\Users\\Vilhe\\IdeaProjects\\Oblig7\\Lempel-Ziv Compression\\lib\\FileOutTest.txt");
 
-        lempelZivCompression.compress();
-
+        lempelZivCompression.findSimilarBytes();
 
     }
 
